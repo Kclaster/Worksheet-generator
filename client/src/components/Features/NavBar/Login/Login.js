48ef2eb43fa4e8ab2ab772e81cfc6a7d5cd21974
patrickj188@gmail.com <Patrickj188@gmail.com>
2019-05-06 16:10:41 -0500

    new files

--- client/src/components/Features/NavBar/Login/Login.js
+++ client/src/components/Features/NavBar/Login/Login.js
@@ -3,11 +3,49 @@
 import React from "react";
 import "./Login.css";
 import MainPages from "../../../MainPages";
-import HomePage from "../../../MainPages/HomePage/HomePage";
+import HomePage from "../../../MainPages/HomePage/HomePage.js";
 import UserPage from "../../../MainPages/UserPage";
 import { GoogleLogin } from "react-google-login-component";
 
 class Login extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     displayUserPage: false
+  //   };
+  // }
+
+  // handleClick = () => {
+  //   this.setState({ displayUserPage: !this.state.displayUserPage });
+  // };
+
+  // render() {
+  //   return (
+  //     <div className="loginbox">
+  //       <form action="/login" method="post">
+  //         <div className="form-group">
+  //           <label>Email</label>
+  //           <br />
+  //           <input type="text" className="form-control" name="username" />
+  //         </div>
+  //         <div className="form-group">
+  //           <label>Password</label>
+  //           <br />
+  //           <input type="text" className="form-control" name="password" />
+  //         </div>
+  //       </form>
+  //       <div>
+  //         <button type="submit" onClick={this.handleClick}>
+  //           Login Button
+  //         </button>
+  //         <button type="register">Register</button>
+  //       </div>
+
+  //       {this.state.displayUserPage ? <UserPage /> : <HomePage />}
+  //     </div>
+  //   );
+  // }
+
   constructor(props, context) {
     super(props, context);
     this.state = {
@@ -47,6 +85,22 @@ class Login extends React.Component {
         {this.state.displayUserPage ? <UserPage /> : <HomePage />}
       </div>
         
+        /* <GoogleLogin
+          clientId="1073244498652-n2m4u3fru7io3pncs484c72qh35sp1u4.apps.googleusercontent.com"
+          render={renderProps => (
+            <button
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+            >
+              This is my custom Google button
+            </button>
+          )}
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+      </div> */
     );
   }
 }
