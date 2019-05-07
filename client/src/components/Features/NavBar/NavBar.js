cf4c770b38ad8f9b3cb806d3068622846f3ff6fe
Cho <YXC5125@alixarx.com>
2019-05-06 21:38:02 -0500

    checking in login with tokens

--- client/src/components/Features/NavBar/NavBar.js
+++ client/src/components/Features/NavBar/NavBar.js
@@ -1,35 +1,75 @@
-import React from "react";
-import Login from "./Login/Login.js";
-import "./NavBar.css";
-// import AboutContainer from "../../MainPages/HomePage/AboutContainer/AboutContainer";
+import React, { Component } from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import Login from './Login/Login.js'
 import HomePage from "../../MainPages/HomePage/homePage";
 
-class NavBar extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      displayUserPage: false
-    };
-  }
+class NavBarLogin extends Component {
+    logOut(e) {
+        e.preventDefault()
+        localStorage.removeItem('usertoken')
+        this.props.history.push(`/`)
+    }
 
-  render() {
-    return (
-      <div>
-        <section>
-          
-          <ul className ="header">
-            <li>
-            <Login />
-          </li>
-          </ul>
-        </section>
+    render() {
+        const loginRegLink = (
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link to="/login" className="nav-link">
+                        Login
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/register" className="nav-link">
+                        Register
+                    </Link>
+                </li>
+            </ul>
+        )
+        const userLink = (
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link to="/profile" className="nav-link">
+                        User
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <a href="" onClick={this.logOut.bind(this)} className="nav-link">
+                        Logout
+                    </a>
+                </li>
+            </ul>
+        )
 
-        <section>
-          <HomePage />
-        </section>
-      </div>
-    );
-  }
+        return (
+          <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded">
+                <button className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbar1"
+                    aria-controls="navbar1"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggle-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse justify-content-md-center"
+                    id="navbar1">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">
+                                Home
+                            </Link>
+                        </li>
+                    </ul>
+                    {localStorage.usertoken ? userLink : loginRegLink}
+                </div>
+                <Login />
+            </nav>
+            <HomePage />
+            </div>
+        )
+    }
 }
 
-export default NavBar;
+export default withRouter(NavBarLogin)
