b2cad56d54c4a7248f048e597a108e0d4c50ce83
Jonathan T <jhtran.dev@gmail.com>
2019-05-08 21:16:13 -0500

    added history tab

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,8 @@
-// import React, { Component } from 'react';
-// import { BrowserRouter, Route } from 'react-router-dom';
-// import Landing from "./components/Landing";
-// import NavBar from "./components/Features/NavBar/NavBar.js";
-// import Footer from "./components/Features/Footer/Footer.js";
-// import Login from "./components/Features/NavBar/Login/Login";
-// import Profile from './components/Features/NavBar/Profile';
-// import Register from './components/Features/NavBar/Register/Register';
-
-// class App extends Component {
-//   render () {
-//     return (
-//       <BrowserRouter>
-//         <div className="App">
-//           <NavBar />
-//           <Route exact path="/" component={Landing} />
-//           <div className="container">
-//             <Route exact path="/login" component={Login} />
-//             <Route exact path="/Register" component={Register} />
-//             <Route exact path="/profile" component={Profile} />
-//           </div>
-//           <Footer />
-//         </div>
-//       </BrowserRouter>
-//     )
-//   }
-// }
-
 import React from "react";
 import MainPages from "./components/MainPages";
 import NavBar from "./components/Features/NavBar/NavBar.js";
 import Footer from "./components/Features/Footer/Footer.js";
 import Login from "./components/Features/NavBar/Login/Login";
-// import Particles from "react-particles-js";
 import ParticlesCustom from "./components/Particles";
 
 class App extends React.Component {
--- client/src/components/Particles.js
+++ client/src/components/Particles.js
@@ -89,20 +89,20 @@ class ParticlesCustom extends React.Component {
             },
             modes: {
               grab: {
-                distance: 400,
+                distance: 100,
                 line_linked: {
                   opacity: 1
                 }
               },
               bubble: {
-                distance: 400,
+                distance: 50,
                 size: 40,
                 duration: 2,
                 opacity: 8,
                 speed: 3
               },
               repulse: {
-                distance: 200,
+                distance: 50,
                 duration: 0.4
               },
               push: {
