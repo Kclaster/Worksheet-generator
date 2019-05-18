40ca43a848b3fc91af973794cdfbebb29baecc0e
Cho <youngtae1386@gmail.com>
2019-05-18 12:26:16 -0500

    made it to syled-components

--- client/src/components/Features/Footer/Footer.js
+++ client/src/components/Features/Footer/Footer.js
@@ -1,26 +1,27 @@
 import React from "react";
+import styled from 'styled-components';
 // import "./Footer.css";
 // import Typography from "@material-ui/core/Typography";
 
-var style = {
-    backgroundColor: "#3f50b5",
-    borderTop: "1px solid #E7E7E7",
-    textAlign: "center",
-    padding: "15px",
-    position: "fixed",
-    left: "0",
-    bottom: "0",
-    height: "10px",
-    width: "100%",
-    color:"white"
-}
+var StyledFooter =  styled.div`
+background-color: #3f50b5,
+border-top: 1px solid #E7E7E7,
+textAlign: center,
+padding: 15px,
+position: fixed,
+left: 0,
+bottom: 0,
+height: 10px,
+width: 100%,
+color: white
+`
 
 
 function Footer() {
     return (
         <div>
-            <div style={style}>Worksheet Generator - 2019  
-            </div>
+            <StyledFooter>Worksheet Generator - 2019  
+            </StyledFooter>
         </div>
     )
 }
