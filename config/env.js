25f64c05ee0c52729702a00981b07093e4958117
Kyle Laster <kyle.laster@gmail.com>
2019-05-18 13:24:56 -0500

    sequelize fix

--- config/env.js
+++ config/env.js
@@ -1,16 +1,23 @@
 var password = ''; // require('./config');
-const env = {
-  database: 'slope_intercept',
-  username: 'root',
-  password: password,
-  host: 'localhost',
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
+var mysql = require('mysql');
 
-module.exports = env;
+let connection_string = '';
+if (process.env.JAWSDB_URL) {
+  console.log('jawsdb is running!!!');
+  connection_string = process.env.JAWSDB_URL;
+} else {
+  connection_string = {
+    database: 'slope_intercept',
+    username: 'root',
+    password: password,
+    host: 'localhost',
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
+}
+module.exports = connection_string;
