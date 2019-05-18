6fa8a72592b5b57c9fca89480b429ab9f6056e41
Jonathan T <jhtran.dev@gmail.com>
2019-05-18 13:53:12 -0500

    fixed particles to take up whole page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,21 @@ import NavBar from "./components/Features/NavBar/NavBar.js";
 import LoginJwt from "./components/Features/NavBar/LoginJwt/Login";
 import Profile from "./components/Features/NavBar/Profile";
 import Register from "./components/Features/NavBar/Register/Register";
+// import ParticlesTop from "../../client/src/components/ParticlesTop";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+margin-top: -50px;
+z-index=-100;
+position: absolute;
+height: 10vh;
+width: 100vw;
+`;
+
+const ZIndex = styled.div`
+z-index = 100;
+opacity: 1;
+`;
 
 class App extends Component {
   render() {
@@ -14,15 +29,19 @@ class App extends Component {
       <div>
         <BrowserRouter>
           <div className="App">
-            <NavBar />
-            <Route exact path="/" component={Landing} />
-            <div className="container">
-              <Route exact path="/login" component={LoginJwt} />
-              <Route exact path="/Register" component={Register} />
-              <Route exact path="/profile" component={Profile} />
-            </div>
-            <Footer />
-            <ParticlesCustom />
+            <Wrapper>
+              <ParticlesCustom />
+            </Wrapper>
+            <ZIndex>
+              <NavBar />
+              <Route exact path="/" component={Landing} />
+              <div className="container">
+                <Route exact path="/login" component={LoginJwt} />
+                <Route exact path="/Register" component={Register} />
+                <Route exact path="/profile" component={Profile} />
+              </div>
+              <Footer />
+            </ZIndex>
           </div>
         </BrowserRouter>
       </div>
